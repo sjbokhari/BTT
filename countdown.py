49b5ba5947407fe0e342ee4db8b4da2346e72209
sjbokhari0 <60392358+sjbokhari0@users.noreply.github.com>
2023-05-01 23:21:25 +0200

    removed unnesseary lines of code

--- countdown.py
+++ countdown.py
@@ -1,24 +1,4 @@
-import os
-import difflib
-import time
 from datetime import datetime
-
-# def countdown(stop):
-#     while True:
-#         difference = stop - datetime.datetime.now()
-#         count_hours, rem = divmod(difference.seconds, 3600)
-#         count_minutes, count_seconds = divmod(rem, 60)
-#         if difference.days == 0 and count_hours == 0 and count_minutes == 0 and count_seconds == 0:
-#             print("FINISH!")
-#             break
-#         print('BA:'
-#               + str(difference.days) + "d"
-#               + str(count_hours) + "h"
-#               + str(count_minutes) + "m"
-#               + str(count_seconds) + "s"
-#               )
-#         time.sleep(1)
-
 
 def countdown_fix(stop):
     difference = stop - datetime.now()
