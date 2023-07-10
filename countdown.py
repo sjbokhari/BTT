3b42ca6fd392b15bc3bf822765f8aa68e6f97028
sjbokhari0 <60392358+sjbokhari0@users.noreply.github.com>
2023-07-10 11:10:47 +0200

    removed kuru, adjusted timer

--- countdown.py
+++ countdown.py
@@ -2,19 +2,19 @@ from datetime import datetime
 
 def countdown_fix(stop: datetime) -> None:
     difference = stop - datetime.now()
-    #count_hours, rem = divmod(difference.seconds, 3600)
+    count_hours, rem = divmod(difference.seconds, 3600)
     #count_minutes, count_seconds = divmod(rem, 60)
     if difference.days < 0:
-        print("日本へようこそ")
+        print("Vacation")
     else:
-        print(  str(difference.days) + "日" #"d"
-            #+ str(count_hours) + "h"
+        print(  str(difference.days) + "d"
+            + str(count_hours) + "h"
             #+ str(count_minutes) + "m"
             #+ str(count_seconds) + "s"
             )
 
 def main() -> None:
-    end_time = datetime(2023, 7, 19, 17, 00, 0)
+    end_time = datetime(2023, 7, 20, 15, 00, 0)
     countdown_fix(end_time)
 
 if __name__ == '__main__':
